e0db953a6a4a94b355e135b0dd5d061860a385cb
jeremyleishman <jeremyl73@hotmail.com>
2020-06-07 22:15:01 -0700

    adding Get-STFolderSize.ps1

---
+++ Get-STFolderSize.ps1
@@ -0,0 +1,9 @@
+# Get-STFolderSize.ps1
+
+<#
+.Synopsis
+    This script will return the following the file size of specified folder in GB:
+ #>
+
+ Get-STFolderSize -Path \\server1\bigfolder | Select TotalGBytes
+ 
