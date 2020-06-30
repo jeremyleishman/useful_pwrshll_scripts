3ba3f646e493108aefeae6f3e1569e816b3bd244
jeremyleishman <jeremyl73@hotmail.com>
2020-06-30 10:17:02 -0700

    Adding cim-instance.ps1

---
+++ cim-instance.ps1
@@ -0,0 +1,6 @@
+# cim-instance.ps1
+
+$ComputerName = Read-Host "Enter Computer Name"
+
+Get-CimInstance -ComputerName $ComputerName -ClassName Win32_OperatingSystem |
+Select-Object CSName,LastBootUpTime
